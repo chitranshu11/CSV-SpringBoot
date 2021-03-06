2b6d413be9d8b3307bf5b2f60e1b731c3305772b
Chitranshu Gour <chitranshugour67@gmail.com>
2021-03-06 18:42:47 +0530

    Minor Changes

--- saveo/src/main/java/com/chitranshu/saveo/json/response/MedicineResponse.java
+++ saveo/src/main/java/com/chitranshu/saveo/json/response/MedicineResponse.java
@@ -1,20 +1,9 @@
 package com.chitranshu.saveo.json.response;
 
-import java.util.List;
-
 public class MedicineResponse {
 	private boolean status;
 	private String message;
-	
-	private List<String> medicineNames;
-	
-	
-	public List<String> getMedicineNames() {
-		return medicineNames;
-	}
-	public void setMedicineNames(List<String> medicineNames) {
-		this.medicineNames = medicineNames;
-	}
+
 	public boolean isStatus() {
 		return status;
 	}
